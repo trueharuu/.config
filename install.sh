bbb5b7a56b23c4ef363e82676006d01896a831c2
mina <trueharuu@gmail.com>
2026-03-06 14:51:58 -0600

    t

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo pacman -S \
+    hyprland \
+    hyprpaper \
+    alacritty \
+    firefox \
+    grim \
+    slurp \
+    wl-clipboard
+
+yay -S \
+    grimblast \
+    dunst \
+    tofi
+
+sudo systemctl --user enable pipewire pipewire-pulse wireplumber
